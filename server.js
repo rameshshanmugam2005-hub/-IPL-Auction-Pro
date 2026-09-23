2452b1816250ad46c22be96d97584df167d2d8bb
Ramesh S <rameshshanmugam2005@gmail.com>
2026-09-23 13:44:15 +0530

    Remove unused fs import
    Removed unused 'fs' import from server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import path from "path";
 import { createServer as createViteServer } from "vite";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
-import fs from "fs";
 
 dotenv.config();
 
